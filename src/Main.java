5d5b2bb47f404ec6d6a3f8e6aca19426577ae368
k33g_org <ph.charriere@gmail.com>
2012-12-29 08:00:54 +0100

    externalize routes to Router

--- src/Main.java
+++ src/Main.java
@@ -4,106 +4,14 @@
  */
 
 import static spark.Spark.*;
-import spark.*;
+import routes.Router;
 import org.k33g.helpers.*;
 
-
 public class Main implements spark.servlet.SparkApplication {
-
-    private static void routes() {
-        N3rd.about();
-
-        /*--- Groovy Support ---*/
-        Groovy.setScriptsPath("gscripts");
-        try {
-            Groovy.iniScriptEngine();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        /*----------------------*/
-
-        get(new Route("/groovy/us") {
-            @Override
-            public Object handle(Request request, Response response) {
-                return controllers.GroovyDemo.helloUS(request, response);
-            }
-        });
-
-        get(new Route("/groovy/fr") {
-            @Override
-            public Object handle(Request request, Response response) {
-                return controllers.GroovyDemo.helloFR(request, response);
-            }
-        });
-
-        /* get All Humans */
-        get(new Route("/groovy/humans") {
-            @Override
-            public Object handle(Request request, Response response) {
-                return controllers.GroovyDemo.allHumans(request, response);
-            }
-        });
-
-        get(new Route("/about") {
-            @Override
-            public Object handle(Request request, Response response) {
-                return controllers.Application.about(request, response);
-            }
-        });
-
-        get(new Route("/about.json") {
-            @Override
-            public Object handle(Request request, Response response) {
-                return controllers.Application.aboutJSON(request, response);
-            }
-        });
-
-        /* get one Human by id */
-        get(new Route("/humans/:id") {
-            @Override
-            public Object handle(Request request, Response response) {
-                return controllers.Humans.fetch(request, response);
-            }
-        });
-
-        /* get All Humans */
-        get(new Route("/humans") {
-            @Override
-            public Object handle(Request request, Response response) {
-                return controllers.Humans.getAll(request, response);
-            }
-        });
-
-        /* create a Human */
-        post(new Route("/humans") {
-            @Override
-            public Object handle(Request request, Response response) {
-                return controllers.Humans.add(request, response);
-            }
-        });
-
-        /* update a human */
-        put(new Route("/humans/:id") {
-            @Override
-            public Object handle(Request request, Response response) {
-                return controllers.Humans.update(request, response);
-            }
-        });
-
-        /* delete a human */
-        delete(new Route("/humans/:id") {
-            @Override
-            public Object handle(Request request, Response response) {
-                return controllers.Humans.delete(request, response);
-            }
-        });
-
-    }
-
     /* standalone mode */
     public static void main(String[] args)  {
         setPort(9000);
-        routes();
+        Router.routes();
 
         //SERVE STATIC FILES (only needed if standalone mode)
         Assets.setPublicPath("public.n3rd"); //<--- static assets path
@@ -115,7 +23,7 @@ public class Main implements spark.servlet.SparkApplication {
     /* Web Server mode */
     @Override
     public void init() {
-        routes();
+        Router.routes();
         /*
             Try this : mvn jetty:run
          */
