9b31dc8727cdfd581ca97cea3ccf2a61147adecc
k33g_org <ph.charriere@gmail.com>
2013-01-19 09:30:12 +0100

    little refactoring

--- src/org/k33g/helpers/Router.java
+++ src/org/k33g/helpers/Router.java
@@ -1,8 +1,6 @@
 package org.k33g.helpers;
 
 import groovy.lang.Binding;
-import org.k33g.helpers.Groovy;
-
 import java.io.File;
 
 public class Router {
@@ -10,19 +8,19 @@ public class Router {
     public static void routes() {
 
         /*--- Groovy Support ---*/
+        /*
         Groovy.setScriptsPath("");
         try {
             Groovy.iniScriptEngine();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+        */
         /*----------------------*/
 
         String groovyFile;
         File folder = new File("routes");
         File[] listOfFiles = folder.listFiles();
-
-        //System.out.println(Json.toJson(listOfFiles));
 
         for (int i = 0; i < listOfFiles.length; i++)
         {
